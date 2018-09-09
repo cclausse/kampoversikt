0ddab30b3caf0d157633777891c84e6f50b7583d
Carsten Claussen <cclausse@gmail.com>
2018-09-09 15:57:57 +0200

    Added some testfiles

---
+++ 04-lists.py
@@ -0,0 +1,14 @@
+my_list_1=[1,2,3,4]
+print(len(my_list_1))
+print(my_list_1[3:])
+
+my_list_2=[5,6,7,8,9]
+
+print(my_list_1 + my_list_2)
+
+my_list_2[1] = 'test'
+print(my_list_2)
+my_list_2.append('hei')
+print(my_list_2)
+my_list_2.pop()
+print(my_list_2)
---
+++ indexing.py
@@ -0,0 +1,35 @@
+import sys
+# mystring = 'Hello World'
+# print(mystring[1])
+# mystnumbersring = 'abcdefghijklmn'
+# mynumbers = '1234567890'
+# print(mystring[2:])
+# print(mystring[:3])
+# print(mystring[5:8])
+# print(mystring[1:3])
+# # stepsize
+# # print(mystring[start:stop:stepsize])
+# print(mystring[::2])
+# print(mynumbers[::2])
+
+# mystring = 'Hello World'
+# print(mystring[8])
+print('tinker'[1:4])
+
+navn = 'Sam'
+# navn[0]='P'
+# print('Carsten'*10)
+x ='carsten'
+print(x.upper())
+print(x.lower())
+x='hei dette er en test'
+print(x.split('e'))
+print('Dette {0} en {1} på {2} av {3} i {4}'.format('er', 'test', 'bruk', 'variable', 'strings'))
+result = 1000/7777
+print('Resultat er {r:1.3f}'.format(r=result))
+print('Resultat er {r:10.3f}'.format(r=result)) #{value:width.precision}
+print('Resultat er {r:10.4f}'.format(r=result)) #{value:width.precision}
+
+name = 'Carsten'
+age = 55
+print(f'Hei, jeg heter {name} og jeg er {age} år!')
